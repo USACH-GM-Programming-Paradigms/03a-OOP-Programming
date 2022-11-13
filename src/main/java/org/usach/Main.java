a6093967b47f3db3c71bc11ff60b1ba275e6387d
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2022-11-12 22:25:07 -0300

    feat(product): add stream api map and filter example with products

--- src/main/java/org/usach/Main.java
+++ src/main/java/org/usach/Main.java
@@ -4,6 +4,7 @@ import org.usach.tda.Product;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,6 +23,26 @@ public class Main {
 
         // Print list of products
         products.forEach(System.out::println);
+
+        // Stream API example
+        // Map
+        var productNames =
+                products.stream()
+                        .map(Product::getName)
+                        .collect(Collectors.toList());
+
+        System.out.println("List of only names (Map example): ");
+        productNames.forEach(System.out::println);
+
+        // Filter
+        var productsPriceGreaterThan400 =
+                products.stream()
+                        .filter(product -> product.getPrice() > 400)
+                        .collect(Collectors.toList());
+
+        System.out.println("List of filtered products by price (Filter example): ");
+        productsPriceGreaterThan400.forEach(System.out::println);
+
 
     }
 }
--- src/main/java/org/usach/tda/Product.java
+++ src/main/java/org/usach/tda/Product.java
@@ -14,6 +14,30 @@ public class Product {
         this.category = category;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
